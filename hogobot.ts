dd9f863d8523d8f61d76bbe534b7d5f5b0375f16
ASST-OP <145039395+ASST-OP@users.noreply.github.com>
2024-10-30 13:51:40 +0900

    hogobot.ts を更新

---
+++ hogobot.ts
@@ -0,0 +1,46 @@
+import { Client } from "jsr:@evex/linejs@1.7.1";
+import { FileStorage } from "jsr:@evex/linejs@1.7.1/storage";
+
+const client = new Client({
+  storage: new FileStorage("./storage.json"),
+});
+client.on("ready", (user) => {
+  console.log(`Logged in as ${user.displayName} (${user.mid})`);
+});
+client.on("pincall", (pincode) => {
+  console.log(`pincode: ${pincode}`);
+});
+client.on("update:cert", d => console.log("cert:", d))
+client.on("update:authtoken", (d) => console.log("AuthToken:", d))
+
+await client.login({
+  device: "IOSIPAD",
+  authToken: "FJzLJer7Uh2BIHa1ATqb.tQ7bqB3AvpZ91q2EQoGOwW.qlZRgj3+vJ+Aj6y8bppSK8pjU3L9RsMQBpB+VFMdakQ=",
+  polling: [],
+});
+const syncToken: Record<string, string> = {};
+
+const squareChatMids = ["m6fb395949c79240780f37668e5f898a9"];
+
+while (true) {
+  for (const squareChatMid of squareChatMids) {
+    const response = await client.fetchSquareChatEvents({
+      squareChatMid,
+      syncToken: syncToken[squareChatMid],
+    });
+    syncToken[squareChatMid] = response.syncToken;
+    for (const event of response.events) {
+      if (
+        event.type === "RECEIVE_MESSAGE" &&
+        event.payload.receiveMessage
+      ) {
+        console.log(event.payload.receiveMessage.squareMessage.message.text)
+        if (event.payload.receiveMessage.squareMessage.message.text === "非表示にしたメッセージです。") {
+          console.log("delete:", event.payload.receiveMessage.squareMessage.message._from, new Date(event.payload.receiveMessage.squareMessage.message.deliveredTime as number).toLocaleTimeString())
+          await client.destroySquareMessage({ messageId: event.payload.receiveMessage.squareMessage.message.id, squareChatMid })
+        }
+      }
+    }
+  }
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+}
