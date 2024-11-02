2dc645421465bcbb932c0a3ed1c620e7ac1d9c6c
ASST-OP <145039395+ASST-OP@users.noreply.github.com>
2024-11-02 14:05:57 +0900

    Update and rename bot-ver5.ts to bot-ver.5.ts

--- bot-ver.5.ts
+++ bot-ver.5.ts
@@ -23,7 +23,7 @@ const syncToken: Record<string, string> = {};
 
 const squareChatMids = ["m6fb395949c79240780f37668e5f898a9", "mcfa28b4d1dd65f35ca21547072c52c14", "m48efef63a86cdc09742195c2a7e45aaf", "m97efcadbc6b4d341e02e7481e990bbf5", "mb1001dbfbe18a763a2d07db64fbf8b6f", "m5fce012dcaee113159482752aeac7a08"];
 
-await client.sendSquareMessage({ squareChatMid: "m6fb395949c79240780f37668e5f898a9", text: `[！]保護bot ver.4起動！
+await client.sendSquareMessage({ squareChatMid: "m6fb395949c79240780f37668e5f898a9", text: `[！]保護bot ver.5起動！
 ${currentDateTime}`});
 
 const sendTimes = ["06:00", "07:00", "08:00", "10:00", "12:00", "14:00", "15:00", "16:00", "17:00", "18:00", "20:00", "22:00", "23:00", "00:00"];
@@ -32,7 +32,7 @@ async function checkAndSend() {
   const now = new Date();
   const currentTime = now.toLocaleTimeString("ja-JP", { hour12: false, hour: "2-digit", minute: "2-digit" });
   if (sendTimes.includes(currentTime)) {
-  await client.sendSquareMessage({ squareChatMid: "m6fb395949c79240780f37668e5f898a9", text: `[！]保護bot ver.4稼働中！
+  await client.sendSquareMessage({ squareChatMid: "m6fb395949c79240780f37668e5f898a9", text: `[！]保護bot ver.5稼働中！
 ${currentTime}` });}
 setTimeout(checkAndSend, 60 * 1000);}
 checkAndSend();
@@ -54,7 +54,7 @@ while (true) {
           console.log("delete:", event.payload.receiveMessage.squareMessage.message._from, new Date(event.payload.receiveMessage.squareMessage.message.deliveredTime as number).toLocaleTimeString())
           await client.destroySquareMessage({ messageId: event.payload.receiveMessage.squareMessage.message.id, squareChatMid })}
           else if (typeof event.payload.receiveMessage.squareMessage.message.text === "string") {
-          if (event.payload.receiveMessage.squareMessage.message.text.length >= 750) {
+          if (event.payload.receiveMessage.squareMessage.message.text.length >= 500) {
           console.log("delete:", event.payload.receiveMessage.squareMessage.message._from, new Date(event.payload.receiveMessage.squareMessage.message.deliveredTime as number).toLocaleTimeString())
           await client.destroySquareMessage({ messageId: event.payload.receiveMessage.squareMessage.message.id, squareChatMid })};
         }
