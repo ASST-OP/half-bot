edc8209e86e031d3b3122e28c9752e9ef1f43a03
ASST-OP <145039395+ASST-OP@users.noreply.github.com>
2024-11-02 13:37:36 +0900

    Update and rename hogobot2.7.ts to hogobot2.8.ts

--- hogobot2.8.ts
+++ hogobot2.8.ts
@@ -41,7 +41,7 @@ while (true) {
         if (event.payload.receiveMessage.squareMessage.message.text === "非表示にしたメッセージです。") {
           console.log("delete:", event.payload.receiveMessage.squareMessage.message._from, new Date(event.payload.receiveMessage.squareMessage.message.deliveredTime as number).toLocaleTimeString())
           await client.destroySquareMessage({ messageId: event.payload.receiveMessage.squareMessage.message.id, squareChatMid })
-        } else if (event.payload.receiveMessage.squareMessage.message.text === "string") {
+        } else if (typeof event.payload.receiveMessage.squareMessage.message.text === "string") {
           if (event.payload.receiveMessage.squareMessage.message.text.length >= 750) {
           console.log("delete:", event.payload.receiveMessage.squareMessage.message._from, new Date(event.payload.receiveMessage.squareMessage.message.deliveredTime as number).toLocaleTimeString())
           await client.destroySquareMessage({ messageId: event.payload.receiveMessage.squareMessage.message.id, squareChatMid })};
